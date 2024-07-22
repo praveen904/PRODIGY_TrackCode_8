b2589f459d07f94ac3760c9f5d883fdd6c7b5a1e
Praveen kumar <126907923+praveen904@users.noreply.github.com>
2024-07-22 18:02:05 +0530

    login php

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <?php
+    $username=$_POST["username"];
+    $password=$_POST["password"];
+ 
+
+    $conn=new mysqli("localhost","root","","praveen");
+
+    if($conn->connect_error){
+        die("Connection filed!!".$conn->connect_error);
+    }
+    else{
+        echo "Connection Successfull !!!<br><br>";
+    }
+    $stmp=$conn->prepare("SELECT*FROM praveen.users WHERE username=? and password=?");
+    $stmp->bind_param("ss",$username,$password);
+
+    if($stmp->execute()===TRUE){
+        $res=$stmp->get_result();
+        if($res->num_rows > 0){
+            header("location:dashboard.html");
+            exit();
+        }
+        else{
+            echo "you don't have account<br><br>";
+            echo "<a href='signin.html'>sign in</a>";
+            
+        }
+    }
+    else{
+        echo"select failed".$stmp->error;
+        
+    }
+    ?>
+</body>
+</html>
